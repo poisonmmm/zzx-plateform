861b9cb35eaf9a8bb25ded87b1ac131ac8b33c1b
luming <2839247545@qq.com>
2023-08-11 10:14:49 +0800

    [add]add wsconfig

---
+++ src/main/java/com/association/ws/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.association.ws;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author LUMING
+ * @since 2023/3/21 10:20
+ */
+@Configuration
+public class WebSocketConfig {
+    /**
+     * 	注入ServerEndpointExporter，
+     * 	这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
+
